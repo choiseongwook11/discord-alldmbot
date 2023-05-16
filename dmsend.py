f723d32623f98340e7e822a0e6e7b1f7a3f9f806
최성욱 <78180994+korash1111@users.noreply.github.com>
2023-05-16 19:54:34 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -1,6 +1,4 @@
 ﻿
-# 아람쓰#5050 또는 아람#5920 : 전체디엠봇 소스
-# 영상보고 모르는점 있을시 유튜브 댓글또는 디엠주세요
 
 
 import discord
@@ -12,7 +10,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇실행이 시작되었습니다(24시간 온라인).")
-    game = discord.Game('스카니아서버 관리')
+    game = discord.Game('서버 관리')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
