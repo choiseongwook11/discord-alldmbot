737d98b73d081fcb4a799b43411e8f91f8ddfc74
seongwook Choi <78180994+korash1111@users.noreply.github.com>
2023-12-12 17:07:03 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -1,6 +1,3 @@
-﻿
-
-
 import discord
 import asyncio
 import datetime
@@ -9,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("봇실행이 시작되었습니다(24시간 온라인).")
+    print("봇실행이 시작되었습니다.")
     game = discord.Game('서버 관리')
     await client.change_presence(status=discord.Status.online, activity=game)
 
@@ -20,16 +17,17 @@ async def on_message(message):
         for i in message.guild.members:
             if i.bot == True:
                 pass
-            else:
+                else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == 669422575432105984:
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="서버")
-                        embed.add_field(name="전체 알림", value=msg, inline=True)
-                        embed.set_footer(text=f"서버")
-                        await i.send(embed=embed)
-                except:
-                    pass
+                    if message.author.id == 디스코드ID를 적기!!:
+                        if message.author.id == 669422575432105984:
+                            embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="서버")
+                            embed.add_field(name="서버 전체 알림", value=msg, inline=True)
+                            embed.set_footer(text=f"서버")
 
 
-client.run('bot key')
+            async def on_message(message):
+                pass
+
+            client.run('봇의 토큰을 넣어주세요')
